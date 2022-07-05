24c25eb639cb5c24109bea8defe5598ef71f59cd
Hannah <hfhealy@gmail.com>
2022-07-05 15:39:06 -0700

    finished project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import $ from 'jquery';
-// var $ = require('jquery');
-$('div').text('Webpack is working!');
+// import $ from 'jquery';
+// // var $ = require('jquery');
+// $('div').text('Webpack is working!');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById('root');
+const sampleComponent = <div> React and webpack are working!</div>;
+
+ReactDOM.render(sampleComponent, rootElement);
